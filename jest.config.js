f05c388e75fe4e654dc665ee7c0cb3ecf14d4990
FrankMugagga <frank.mugagga@gmail.com>
2023-03-31 09:40:02 +0300

    Added a jest.config.js file to enable testing with jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // other configuration options...
+    testEnvironment: 'jsdom',
+
+    moduleNameMapper: {
+      "\\.(scss)$": "<rootDir>/__mocks__/styleMock.js"
+    },
+    
+  };
