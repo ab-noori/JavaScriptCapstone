652316c12a7bbddaa20b7c12c9bdbe022e80f04b
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-01 08:41:49 +0430

    Add the main script file.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,48 @@
 import './styles/main.scss';
 import logo5 from './assets/logo5.png';
-import { getMovie, involve } from './modules/getMovies.js';
+import { getMovie } from './modules/getMovies.js';
+import showSelectedMovie from './modules/popUp.js';
 
 const logo = document.getElementById('logo');
 logo.src = logo5;
 
 const container = document.querySelector('.grid_container');
 
-async function display(data) {   
-    const leng = data.length;
-    const totalItems = document.querySelector('.item_number');
-    totalItems.innerHTML = `Movies(${leng})`;
+async function display(data) {
+  const leng = data.length;
+  const totalItems = document.querySelector('.item_number');
+  totalItems.innerHTML = `Movies(${leng})`;
 
-container.innerHTML = '';
-let result = '';
-data.forEach((item) => {
+  container.innerHTML = '';
+  // const result = '';
+  data.forEach((item) => {
     const section = document.createElement('section');
     section.classList.add('sec');
 
-    const movie_img = document.createElement('img');
-    movie_img.classList.add('grid_img');
-    movie_img.src = item.image.medium;
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('grid_img');
+    movieImg.src = item.image.medium;
 
     const spacelikeCont = document.createElement('div');
     spacelikeCont.classList.add('space_like');
 
-    const name = document.createElement('div')
+    const name = document.createElement('div');
     name.innerText = `${item.id}.  ${item.name} `;
 
     const likeCont = document.createElement('div');
     likeCont.classList.add('like_cont');
 
     const likeBtn = document.createElement('button');
-    likeBtn.innerHTML = `<i class="fa-sharp fa-regular fa-heart"></i>`;
+    likeBtn.innerHTML = '<i class="fa-sharp fa-regular fa-heart"></i>';
 
     const likes = document.createElement('div');
     likes.innerText = `${item.id} likes`;
 
     const commentBtn = document.createElement('button');
-    commentBtn.innerText = 'comments'
+    commentBtn.innerText = 'comments';
     commentBtn.classList.add('comment');
 
-    section.appendChild(movie_img);
+    section.appendChild(movieImg);
     section.appendChild(spacelikeCont);
     spacelikeCont.appendChild(name);
     spacelikeCont.appendChild(likeCont);
@@ -51,29 +52,16 @@ data.forEach((item) => {
 
     container.appendChild(section);
     section.appendChild(commentBtn);
-//comment event listener
+    // comment event listener
     commentBtn.addEventListener('click', () => {
-        alert(item.id);
+      showSelectedMovie(item.id);
     });
-
-    
-});
-
+  });
 }
 
 async function show() {
-    let showMovie = await getMovie();
-    display(showMovie);
+  const showMovie = await getMovie();
+  display(showMovie);
 }
 
 show();
-
-
-/*
-const commentBtn = document.querySelector('.comment');
-commentBtn.addEventListener('click', () => {
-    alert(ok)
-})*/
-
-
-
