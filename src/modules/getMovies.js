6a025a6ea7120a8107ccc4d9472d408edc4b5bd7
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-01 08:43:06 +0430

    Add the gibMovie module.

--- src/modules/getMovies.js
+++ src/modules/getMovies.js
@@ -1,21 +1,19 @@
-export const getMovie = async() => {
-        const getMovie = await fetch('https://api.tvmaze.com/shows');
-        const result = await getMovie.json();
-        return result; 
-}
+export const getMovie = async () => {
+  const getMovie = await fetch('https://api.tvmaze.com/shows');
+  const result = await getMovie.json();
+  return result;
+};
 
-export const involve = async() => {
-    try{
-        const involveData = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1sJhZQQrIszrR5zukipa/likes/');
-        const involveResult = await(involveData).json();
-        //return involveResult;
+export const involve = async () => {
+  try {
+    const involveData = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1sJhZQQrIszrR5zukipa/likes/');
+    const involveResult = await (involveData).json();
+    // return involveResult;
 
-        involveResult.forEach(item => {
-            console.log(item.app_id)
-
-        });
-
-    }catch(err){
-        console.log(err);
-    }
-}
+    involveResult.forEach((item) => {
+      console.log(item.app_id);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
